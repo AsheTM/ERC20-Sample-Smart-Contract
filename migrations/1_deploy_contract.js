7695f71bfe1b90ff46280fa678861cbf0663b9c1
AsheTM <haithemalidaoud@gmail.com>
2022-11-18 14:17:11 +0100

    Add configuration in order to migrate to goerli network

---
+++ migrations/1_deploy_contract.js
@@ -0,0 +1,18 @@
+
+const Token = artifacts.require('Token');
+const {
+    TOKEN_DECIMALS, 
+    TOKEN_NAME, 
+    TOKEN_SYMBOL, 
+    TOKEN_TOTAL_SUPPLY, 
+} = process.env;
+
+module.exports = async function(deployer) {
+    await deployer.deploy(
+        Token, 
+        TOKEN_NAME, 
+        TOKEN_SYMBOL, 
+        TOKEN_TOTAL_SUPPLY, 
+        TOKEN_DECIMALS
+    );
+}
